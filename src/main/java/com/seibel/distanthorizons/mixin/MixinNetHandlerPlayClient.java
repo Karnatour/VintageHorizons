e33cbad9c9a74e58550b8d397ae5389751057e97
Vojtěch Šokala <vojtasokala@gmail.com>
2025-07-07 18:23:00 +0200

    Make Worldgen/Import work

---
+++ src/main/java/com/seibel/distanthorizons/mixin/MixinNetHandlerPlayClient.java
@@ -0,0 +1,31 @@
+package com.seibel.distanthorizons.mixin;
+
+import com.seibel.distanthorizons.core.api.internal.ClientApi;
+import com.seibel.distanthorizons.forge.ForgeServerProxy;
+import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.network.play.server.SPacketJoinGame;
+import net.minecraft.util.text.ITextComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(NetHandlerPlayClient.class)
+public class MixinNetHandlerPlayClient
+{
+	
+	@Inject(method = "handleJoinGame", at = @At("RETURN"))
+	private void onHandleJoinGameEnd(SPacketJoinGame packet, CallbackInfo ci)
+	{
+		ClientApi.INSTANCE.onClientOnlyConnected();
+		ForgeServerProxy.connected = true;
+	}
+	
+	@Inject(method = "onDisconnect", at = @At("RETURN"))
+	private void onDisconnectStart(ITextComponent reason, CallbackInfo ci)
+	{
+		ForgeServerProxy.connected = false;
+		ClientApi.INSTANCE.onClientOnlyDisconnected();
+	}
+	
+}
