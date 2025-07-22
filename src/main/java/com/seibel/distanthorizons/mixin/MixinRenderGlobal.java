a4d4f6ca1a97c144291bc3590bdd2b73b4ceca06
Vojtěch Šokala <vojtasokala@gmail.com>
2025-07-22 12:17:15 +0200

    Render LOD only support

--- src/main/java/com/seibel/distanthorizons/mixin/MixinRenderGlobal.java
+++ src/main/java/com/seibel/distanthorizons/mixin/MixinRenderGlobal.java
@@ -1,6 +1,7 @@
 package com.seibel.distanthorizons.mixin;
 
 import com.seibel.distanthorizons.RenderHelper;
+import com.seibel.distanthorizons.core.config.Config;
 import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.BlockRenderLayer;
@@ -14,10 +15,13 @@ public class MixinRenderGlobal
 {
 	@Inject(
 			method = "renderBlockLayer(Lnet/minecraft/util/BlockRenderLayer;DILnet/minecraft/entity/Entity;)I",
-			at = @At("HEAD")
-	)
+			at = @At("HEAD"),
+			cancellable = true)
 	public void onRenderBlockLayer(BlockRenderLayer blockLayerIn, double partialTicks, int pass, Entity entityIn, CallbackInfoReturnable<Integer> cir)
 	{
+		if (Config.Client.Advanced.Debugging.lodOnlyMode.get() && blockLayerIn != BlockRenderLayer.CUTOUT){
+			cir.cancel();
+		}
 		if (blockLayerIn == BlockRenderLayer.SOLID)
 		{
 			RenderHelper.drawLods();
