5d94c65bdf966bebec9b2e92401c2617d7643f1a
Vojtěch Šokala <vojtasokala@gmail.com>
2025-07-22 10:44:36 +0200

    bump version [skip ci]

--- src/main/java/com/seibel/distanthorizons/forge/ForgeMain.java
+++ src/main/java/com/seibel/distanthorizons/forge/ForgeMain.java
@@ -42,7 +42,7 @@ import java.util.function.Consumer;
  * If you are looking for the real start of the mod
  * check out the ClientProxy.
  */
-@Mod(modid = "distanthorizons", name = "DistantHorizons", version = "1.2.1")
+@Mod(modid = "distanthorizons", name = "DistantHorizons", version = "1.2.2")
 public class ForgeMain extends AbstractModInitializer
 {
 	
